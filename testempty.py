9e545af9537d1bfe0083dd01dd7e4a76fb37bb20
ByteKrieger <44577249+ByteKrieger@users.noreply.github.com>
2024-04-19 12:20:58 +0200

    erster entwurf

---
+++ testempty.py
@@ -0,0 +1,41 @@
+pi = 3.1415
+while True:
+    try:
+        wahl = int(input('Willst du Zylinder:1 oder Kreis:2 ? : '))
+        break
+    except ValueError:
+        print('wiederhole deine Eingabe')
+        continue
+while True:   
+    try:  
+        if wahl == 1:
+            r = float(input('Gib den Radius ein : '))
+            h = float(input('Gib die Höhe ein   : '))
+
+            rechnung = r * r * pi * h
+
+            v = round(rechnung, 2)
+            l = round(rechnung / 1000)
+            print(f' Das Volumen beträgt {v}cm^3 deine benötigte Erdemenge beträgt somit ca {l} Liter.')
+            if l < 25.0:
+                print('du brauchst weniger als 25l Erde!')
+            else:
+                print('Du brauchst soviel Erde das du jetzt pleite bist')
+
+            break
+        if wahl == 2:
+            a = float(input('Gib Seite A an  : '))
+            b = float(input('Gib Seite B an  : '))
+            c = float(input('Gib die Höhe an : '))
+
+            rechnung = a * b * c
+            v = round(rechnung, 2)
+            l = round(rechnung / 1000)
+            print(f' Das Volumen beträgt {v}cm^3 deine benötigte Erdemenge beträgt somit ca {l} Liter.')
+
+            break
+
+    except ValueError:
+        print('Fehlerhafte Eingabe !')
+
+
